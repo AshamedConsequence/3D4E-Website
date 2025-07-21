cc4debd206fa79e7dc02b83869155de7bb337d0a
AshamedConsequence <maxime.morize@outlook.com>
2025-07-21 19:41:00 +0900

    did some stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+
+<nav>
+    <a href="/">Home (to be replaced with the logo)</a>
+    <a href="/about-us">About Us</a>
+    <a href="/projects">Projects</a>
+    <a href="/events">Events</a>
+    <a href="/chat">Chat</a>
+    <a href="/shop">Shop</a>
+</nav>
+
+<slot />
